481f1e88f9a03171ec658582f474ea0d36036c75
JP Perez <jaiperez@microsoft.com>
2022-09-08 12:08:38 -0400

    Testing

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+# Container image that runs your code
+FROM alpine:3.10
+
+RUN apk add --no-cache \
+    bash \
+    curl \
+    git \
+    jq \
+    openssh-client \
+    python3 \
+    py3-pip \
+    py3-setuptools \
+    py3-wheel \
+    && pip3 install --upgrade pip \
+    && pip3 install --upgrade awscli
+    terraform \
+    terraform-docs \
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY entrypoint.sh /entrypoint.sh
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
